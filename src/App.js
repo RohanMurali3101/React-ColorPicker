116b5525c2b65b87a97783b8056ebdb1ec5e21b3
Rohan Murali <rohanmurali2004@gmail.com>
2024-07-02 12:35:07 +0530

    Uploading

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from './Components/Container.js';
+
+function App() {
+  return (
+    <Container/>
+  );
+}
+
+export default App;
